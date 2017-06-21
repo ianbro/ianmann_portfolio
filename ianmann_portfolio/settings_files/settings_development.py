276f5350fb9fade41363135a16458c21433f8b4b
Ian Kirkpatrick <ianmann56@gmail.com>
2017-06-21 19:08:17 -0400

    Setup templates

--- ianmann_portfolio/settings_files/settings_development.py
+++ ianmann_portfolio/settings_files/settings_development.py
@@ -13,7 +13,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'ianmann_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
